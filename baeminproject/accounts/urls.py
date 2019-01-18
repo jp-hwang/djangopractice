de53dc263496e6869b85a4a7e0b68301dcab5da7
jp-hwang <jaepilh77@gmail.com>
2019-01-18 14:24:59 +0900

    account 로그인

--- baeminproject/accounts/urls.py
+++ baeminproject/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf import Path
+from django.conf.urls import url
+from django.contrib.auth import views
 
 urlpatterns =[
-    
+    url('login', views.LoginView.as_view(template_name = 'accounts/login_form.html'), name='login')
 ]
