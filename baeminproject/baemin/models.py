8db99bdb3d621c7c4a151f6510228d4acdf26158
jp-hwang <jaepilh77@gmail.com>
2019-01-18 16:00:10 +0900

    baemin app 모델 추가 migrate

--- baeminproject/baemin/models.py
+++ baeminproject/baemin/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)                 # 이름
+    tel = models.CharField(max_length=20)                   # 전화번호
+    addr = models.CharField(max_length=100)                 # 주소
+
+class Item(models.Model):
+    shop = models.ForeignKey(Shop, on_delete = models.CASCADE)                 # 가게
+    name = models.CharField(max_length=100)                 # 이름
+    price = models.PositiveIntegerField()                # 가격
+
+class Order(models.Model):
+    shop = models.ForeignKey(Shop , on_delete = models.CASCADE)                 # 가게
+    user = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete = models.CASCADE)                 # 주문한 유저
+    item_set = models.ManyToManyField(Item)             # 주문한 상품 목록 (Hint: ManyToManyField)
+    created_at = models.DateTimeField(auto_now_add=True)           # 생성일시
