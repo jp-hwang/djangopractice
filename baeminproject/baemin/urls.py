c31cd385df7ebe6c4faa29dec26b6ce448896c04
jp-hwang <jaepilh77@gmail.com>
2019-01-20 13:38:23 +0900

    usl 수정

--- baeminproject/baemin/urls.py
+++ baeminproject/baemin/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path(r'^$', views.index, name='index'),
-    path(r'(?P<shop_id>\d+)/order/new/$', views.order_new, name='order_new'),
+    path(r'(?P<int:shop_id>\d+)/order/new/$', views.order_new, name='order_new'),
 ]
